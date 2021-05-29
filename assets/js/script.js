2d9fa82de79a422a21b06296b41c276152b5f8a7
Daryl Nauman <darylnauman@gmail.com>
2021-05-28 20:30:21 -0400

    Created event listener for buttons & function to save event descriptions using DOM traversal, jQuery

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,8 @@ var rootEl = $('#root');
 var currentDate = moment().format("dddd, MMM Do")
 $("#currentDay").text(currentDate);
 
+
+
 var schedule = [
     {
         time: '9am',
@@ -43,7 +45,6 @@ var schedule = [
     }
 ];
 
-
 function createRows() {
     for (var i = 0; i < schedule.length; i++) {
 
@@ -53,15 +54,19 @@ function createRows() {
         rowEl.attr('id', schedule[i].time);
         rootEl.append(rowEl);
 
+        // create first column within row - lists time of time block
         var plannerTimeEl = $('<div>');
         plannerTimeEl.addClass('col-1 hour');
         plannerTimeEl.text(schedule[i].time);
         rowEl.append(plannerTimeEl);
 
+        // create second column within row - provides text area that can show description of any events, color set if past, present, future
         var plannerDescriptionEl = $('<textarea>');
         plannerDescriptionEl.addClass('col-10 textarea');
+        plannerDescriptionEl.attr('name', 'eventDescription')
         rowEl.append(plannerDescriptionEl);
 
+        // create third column with row - provides a button to click to save text entered into to localStorage
         var plannerSaveButtonEl = $('<button>');
         plannerSaveButtonEl.addClass('col-1 saveBtn fa fa-lock');
         rowEl.append(plannerSaveButtonEl);
@@ -71,14 +76,36 @@ function createRows() {
 function colorRowDescription() {
     for (var i = 0; i < schedule.length; i++) {
         // set background color of row planner description textbox
-        plannerDescriptionEl.addClass('past');
-        //    plannerDescriptionEl.addClass('present');
-        //    plannerDescriptionEl.addClass('future');
+        // plannerDescriptionEl.addClass('past');
+        // plannerDescriptionEl.addClass('present');
+        // plannerDescriptionEl.addClass('future');
     };
 }
 
+function saveDescription(event) {
+    var buttonEl = event.target;
+    var descriptionEl = $(buttonEl).parent().find('textarea');
+    var description = descriptionEl.val();
+
+    // $(buttonEl).parent().find('textarea').css("background-color","red");
+    
+    console.log(buttonEl);
+    console.log(description);
+    console.log($(buttonEl).parent().attr('id'));
+
+    for (var i=0; i < schedule.length; i++) {
+        if (schedule[i].time === $(buttonEl).parent().attr('id')) {
+            schedule[i].description = description;
+        }
+    };
+    // $('input[name="eventDescription"]');
+};
+
 createRows();
 colorRowDescription();
+
+$("button").on('click', saveDescription);
+
 
 // timeBlocksEl = rootEl.find('.textarea').addClass('future');
 
